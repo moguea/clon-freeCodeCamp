7805bbb0b6d99b572e2b590cd03cad3dddeccd78
Luis Moguea <luismoguea947@gmail.com>
2024-03-07 15:52:18 -0500

    primer commit

---
+++ clon-freecode/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import Testimonio from './componentes/Testimonio'
+
+function App() {
+  return (
+    <div className="App">
+      <div className='contenedor-principal'>
+      <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+      <Testimonio 
+      nombre='Emma Bostian'
+      pais='Suecia'
+      imagen=''
+      cargo='Ingeniería de Software'
+      empresa='Spotify'
+      testimonio='Siempre he tenido problemas para aprender JavaScript. Tomó muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo. de mis sueños como ingeniero de software en Spotify.'
+      />
+      </div>
+    </div>
+  );
+}
+
+export default App;
