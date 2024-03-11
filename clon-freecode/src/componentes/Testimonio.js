9056117de72f98fcedb3100a9835f44c20762708
Luis Moguea <luismoguea947@gmail.com>
2024-03-11 12:46:13 -0500

    Se agregaron nuevos componentes

--- clon-freecode/src/componentes/Testimonio.js
+++ clon-freecode/src/componentes/Testimonio.js
@@ -6,12 +6,17 @@ function Testimonio (props) {
 <div className="contenedor-testimonio  ">
 	<img 
 	className="imagen-testimonio"
-	src={require("../imagenes/testimonio-Emma.png")}
-	alt="foto de Emma"/>
+	src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+	alt="foto de Emma" />
 	<div className="contenedor-texto-testimonio">
-		<p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
-		<p className="cargo-testimonio">{props.cargo}  en  {props.empresa}</p>
-		<p className="texto-testimonio">"{props.testimonio}"</p>
+		<p className="nombre-testimonio">
+			<strong>{props.nombre}</strong> en {props.pais}
+			</p>
+		<p className="cargo-testimonio">
+			{props.cargo}  en <strong>{props.empresa}</strong>
+			</p>
+		<p className="texto-testimonio">"
+		{props.testimonio}"</p>
 
 	</div>
 </div>
